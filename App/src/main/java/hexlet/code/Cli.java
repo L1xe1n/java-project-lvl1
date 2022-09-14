daf7834095bc45dc4c62c43cfa585df5ca2983da
Минин Артем <minin.temka@gmail.com>
2022-09-14 19:05:47 +0500

    3 step

---
+++ App/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void SayYourName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String name = sc.next();
+        System.out.println("Hello, " + name + "!");
+    }
+
+}
